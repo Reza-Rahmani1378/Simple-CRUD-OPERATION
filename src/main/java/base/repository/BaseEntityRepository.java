ec44b4b198d9f08afe56272df9a6cf1ef0859c5d
ASUS CENETR QOM <reza6273757@gmail.com>
2021-08-31 02:00:35 -0700

    add BaseEntityRepository

---
+++ src/main/java/base/repository/BaseEntityRepository.java
@@ -0,0 +1,25 @@
+package base.repository;
+
+import base.domain.BaseEntity;
+
+import javax.persistence.EntityManager;
+import java.io.Serializable;
+import java.util.List;
+
+public interface BaseEntityRepository<E extends BaseEntity<ID>, ID extends Serializable> {
+
+    E save(E e);
+
+    E findById(ID id);
+
+    List<E> findAll();
+
+    void delete(E e);
+
+    Boolean existById(ID id);
+
+    EntityManager getEntityManger();
+
+
+}
+
---
+++ src/main/java/base/repository/impl/BaseEntityRepositoryImpl.java
@@ -0,0 +1,62 @@
+package base.repository.impl;
+
+import base.domain.BaseEntity;
+import base.repository.BaseEntityRepository;
+
+import javax.persistence.EntityManager;
+import java.io.Serializable;
+import java.util.List;
+
+public abstract class BaseEntityRepositoryImpl<E extends BaseEntity<ID>, ID extends Serializable>
+        implements BaseEntityRepository<E, ID> {
+
+    protected final EntityManager entityManager;
+
+    protected BaseEntityRepositoryImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public abstract Class<E> getEntityClass();
+
+    @Override
+    public E save(E e) {
+        if (e.getId() == null) {
+            entityManager.persist(e);
+            return e;
+        } else {
+            return entityManager.merge(e);
+        }
+    }
+
+    @Override
+    public E findById(ID id) {
+        return entityManager.find(getEntityClass(), id);
+    }
+
+    @Override
+    public List<E> findAll() {
+        return entityManager.createQuery(
+                "from " + getEntityClass().getSimpleName(),
+                getEntityClass()
+        ).getResultList();
+    }
+
+    @Override
+    public void delete(E e) {
+        entityManager.remove(e);
+    }
+
+    @Override
+    public Boolean existById(ID id) {
+        return entityManager.createQuery(
+                "select count(id) from " + getEntityClass().getSimpleName() +
+                        " where id := id",
+                Long.class
+        ).setParameter("id", id).getSingleResult() == 1L;
+    }
+
+    @Override
+    public EntityManager getEntityManger() {
+        return entityManager;
+    }
+}
